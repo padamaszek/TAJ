1ec7e199a3b2e503f9fe71a40f05affbe7cc9fea
padamaszek <Ursus707@gmail.com>
2015-03-10 21:33:06 +0100

    added Lab2 Zad1 implementation

---
+++ Lab2/Zad21/src/NieudanyPsikusException.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Hunter on 2015-03-10.
+ */
+public class NieudanyPsikusException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -4197380075619961717L;
+    //Parameterless Constructor
+    public NieudanyPsikusException() {}
+    //Constructor that accepts a message
+    public NieudanyPsikusException(String message)
+    {
+        super(message);
+    }
+}
---
+++ Lab2/Zad21/src/PsikusImp.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Hunter on 2015-03-10.
+ */
+    public interface PsikusImp {
+        Integer cyfrokrad(Integer liczba);
+        Integer hultajchochla(Integer liczba) throws NieudanyPsikusException;
+        Integer nieksztaltek(Integer liczba);
+    }
