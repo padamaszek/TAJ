c08b20153bf5abef308b938e39484b7a463e5577
padamaszek <Ursus707@gmail.com>
2015-03-11 13:35:07 +0100

    refactor and simple tests

--- Lab2/Zad21/src/Psikus.java
+++ Lab2/Zad21/src/Psikus.java
@@ -5,16 +5,16 @@ import java.util.Random;
 /**
  * Created by Hunter on 2015-03-10.
  */
-public class Psikus implements PsikusImp{
+public class Psikus implements PsikusImp {
 
     @Override
-    public Integer cyfrokrad (Integer liczba){
+    public Integer cyfrokrad(Integer liczba) {
         //Split of Integer to string
         String number = String.valueOf(liczba);
         //List<Integer> digits = new ArrayList<Integer>(); easier way
         String[] digits = number.split("(?<=.)");
         //return null if only 1 element
-        if(digits.length <= 1){
+        if (digits.length <= 1) {
             return null;
         }
 
@@ -22,19 +22,17 @@ public class Psikus implements PsikusImp{
 
         int randomize = random.nextInt(digits.length);
         //loop to delete an element(Array list would be easier ArrayList got .remove method)
-        for (int i = 0; i < digits.length; i++)
-        {
-            if (digits[i].equals(randomize))
-            {
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i].equals(randomize)) {
                 digits[i] = null;
                 break;
             }
         }
         //adding array to new integer
         int i;
-        int num=0;
-        for(i=0;i<digits.length;i++){
-            num = 10*num + Integer.parseInt(digits[i]);
+        int num = 0;
+        for (i = 0; i < digits.length; i++) {
+            num = 10 * num + Integer.parseInt(digits[i]);
         }
         return num;
     }
@@ -50,9 +48,9 @@ public class Psikus implements PsikusImp{
         int randomize = random.nextInt(digits.length);
         int randomize2 = random.nextInt(digits.length);
 
-        String pom,pom2;
+        String pom, pom2;
         try {
-            if(digits.length <= 1) {
+            if (digits.length <= 1) {
                 throw new NieudanyPsikusException("Wrong Length (it should be above 1)");
             }
             //switch
@@ -61,17 +59,18 @@ public class Psikus implements PsikusImp{
             digits[randomize] = pom2;
             digits[randomize2] = pom;
 
-        } catch(NieudanyPsikusException e) {
+        } catch (NieudanyPsikusException e) {
             System.out.println(e.getMessage());
         }
         //adding array to new integer
         int i;
-        int num=0;
-        for(i=0;i<digits.length;i++){
-            num = 10*num + Integer.parseInt(digits[i]);
+        int num = 0;
+        for (i = 0; i < digits.length; i++) {
+            num = 10 * num + Integer.parseInt(digits[i]);
         }
         return num;
     }
+
     @Override
     public Integer nieksztaltek(Integer liczba) {
         //Split of Integer to string
@@ -83,7 +82,7 @@ public class Psikus implements PsikusImp{
         List<Integer> three = new ArrayList<Integer>();
         List<Integer> six = new ArrayList<Integer>();
         List<Integer> seven = new ArrayList<Integer>();
-        for(i=0;i<digits.length;i++) {
+        for (i = 0; i < digits.length; i++) {
             if (digits[i] == "3") {
                 three.add(i);
             }
@@ -94,22 +93,31 @@ public class Psikus implements PsikusImp{
                 seven.add(i);
             }
         }
-            int randomize;
-            Random random = new Random();
+        //if empty return argument
+        if (three.size() == 0 && six.size() == 0 && seven.size() == 0) {
+            return liczba;
+        }
 
+        int randomize;
+        Random random = new Random();
+        if (three.size() != 0) {
             randomize = random.nextInt(three.size());
             digits[randomize] = "8";
+        }
+        if (six.size() != 0) {
             randomize = random.nextInt(six.size());
             digits[randomize] = "9";
+        }
+        if (seven.size() != 0) {
             randomize = random.nextInt(seven.size());
             digits[randomize] = "1";
+        }
+        //adding array to new integer
+        int num = 0;
+        for (i = 0; i < digits.length; i++) {
+            num = 10 * num + Integer.parseInt(digits[i]);
+        }
 
-            //adding array to new integer
-            int num=0;
-            for(i=0;i<digits.length;i++){
-                num = 10*num + Integer.parseInt(digits[i]);
-            }
-            return num;
+        return num;
     }
-
 }
---
+++ Lab2/Zad21/src/PsikusTest.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class PsikusTest {
+
+    private Psikus psikus;
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    //cyfrokrad Tests
+    @Test
+    public void cyfrokradNotNullReturn() {
+        Assert.assertNotNull(psikus.cyfrokrad(123));
+    }
+
+    @Test
+    public void cyfrokradReturnNull() {
+        Assert.assertNull(psikus.cyfrokrad(5));
+    }
+
+    @Test
+    public void cyfrokradRemoveofOneNumber() {
+        Assert.assertEquals(2, psikus.cyfrokrad(169).toString().length());
+    }
+
+    //chujtajchochla Test
+    @Test
+    public void testHultajchochla() throws Exception {
+
+
+    }
+
+    //Nieksztaltek Tests:
+    @Test
+    public void nieksztaltekSameNumberNothingToSwap() {
+        Assert.assertEquals(124, psikus.nieksztaltek(124).intValue());
+    }
+
+    @Test
+    public void nieksztaltekDiffrentNumberSwap() {
+        Assert.assertNotEquals(136, psikus.nieksztaltek(136).intValue());
+    }
+
+}
